d329e291413e3b7f3d3558cd34c0afcc070b0840
Juan Carlos Fernández <juan.carlos.fernandez@ricoh.es>
2025-05-09 16:17:12 +0200

    Add routing and components for basic, custom, numbers, and uncommon pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'basic',
+    title: 'Pipes basicos',
+    loadComponent: () => import('./pages/basic-page/basic-page.component'),
+  },
+  {
+    path: 'custom',
+    title: 'Pipes custom',
+    loadComponent: () => import('./pages/custom-page/custom-page.component'),
+  },
+  {
+    path: 'numbers',
+    title: 'Pipes numbers',
+    loadComponent: () => import('./pages/numbers-page/numbers-page.component'),
+  },
+  {
+    path: 'uncommon',
+    title: 'Pipes no comunes',
+    loadComponent: () => import('./pages/uncommon-page/uncommon-page.component'),
+  },
+  {
+    path: '**',
+    redirectTo: 'basic'
+  }
+];
