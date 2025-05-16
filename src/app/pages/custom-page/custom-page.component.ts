5000b94918ec446521f029939bf08a883fb25ec5
Juan Carlos Fernández <juan.carlos.fernandez@ricoh.es>
2025-05-16 11:19:54 +0200

    feat: enhance custom page with new pipes and localization support
    - Added localization support for Spanish and French in app.config.ts.
    - Updated custom-page.component.html to showcase Slice, Json, and KeyValue pipes with example data.
    - Refactored custom-page.component.ts to include a signal-based clients array and a promise for asynchronous data handling.

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,8 +1,42 @@
-import { Component } from '@angular/core';
+import { JsonPipe, KeyValuePipe, SlicePipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
+interface Client {
+  name: string;
+  surname: string;
+  address: string;
+  age: number;
+}
 @Component({
   selector: 'app-custom-page',
-  imports: [],
+  imports: [JsonPipe, SlicePipe, KeyValuePipe],
   templateUrl: './custom-page.component.html',
 })
-export default class CustomPageComponent { }
+export default class CustomPageComponent {
+
+  clients = signal<Client[]>([{
+    name: 'juan',
+    surname: 'fernandez',
+    address: 'Toronto',
+    age: 31
+  }, {
+    name: 'crist',
+    surname: 'mir',
+    address: 'Canada',
+    age: 31
+  }
+  ]);
+
+  profile = {
+    name: 'juan',
+    address: 'Barcelona, Spain',
+    age: 50
+  };
+
+  promiseValue: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data en la promesa');
+      console.log('Promesa finalizada')
+    }, 3500);
+  });
+}
